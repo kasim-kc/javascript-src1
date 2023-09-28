a1eea18b5fc2c835f3a42c1f6643e1dedad2b420
kasim-kc <133113806+kasim-kc@users.noreply.github.com>
2023-09-28 03:27:20 +0000

    Modified Conditionals

--- basics/conditionals.js
+++ basics/conditionals.js
@@ -42,5 +42,29 @@ if(number == 0){
 }
 */
 
+// Switch-Case
+/*
+const day = 3;
+switch(day){
+    case 1:{
+        console.log('Monday');
+        break;
+    }
+    case 2:{
+        console.log('Tuesday');
+        break;
+    }
+    case 3:{
+        console.log('Wednesday');
+        break;
+    }
+    case 4:{
+        console.log('Thursday');
+        break;
+    }
+    default:{
+        console.log('Any Other Day');
+    }
+}
 
-
+*/
