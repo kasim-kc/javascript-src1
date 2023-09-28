17f90904bfd998098193b2299672fcf8af92abee
kasim-kc <133113806+kasim-kc@users.noreply.github.com>
2023-09-28 03:09:19 +0000

    Learnt Conditionals

---
+++ basics/conditionals.js
@@ -0,0 +1,46 @@
+console.log("GOING TO LEARN CONDITIONALS!");
+
+// If-else Condition
+/*
+let age = 13;
+
+if(age >= 18){
+    console.log("You can vote in election")
+}else{
+    console.log("You can't vote in election")
+}
+
+*/
+
+// If - Else-If
+/*
+let trafficLight = "yellow";
+if(trafficLight == "green"){
+    console.log('Go Ahead');
+}
+else if(trafficLight == "red"){
+    console.log('Stop');
+}
+else{
+    console.log('Slow Down');
+}
+*/
+
+// Even - Odd
+/*
+let number = 0;
+
+if(number == 0){
+    console.log('Number is 0');
+}else{
+    if(number % 2 == 0){
+        console.log('Even');
+    }
+    else {
+        console.log('Odd');
+    }
+}
+*/
+
+
+
