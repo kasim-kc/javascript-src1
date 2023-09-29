059c27efb5d6544e0732bd394ed33c3ba58e960f
kasim-kc <133113806+kasim-kc@users.noreply.github.com>
2023-09-29 05:29:40 +0000

    Datatypes Declaration and Summary

--- basics/variables.js
+++ basics/variables.js
@@ -15,5 +15,35 @@ Prefer not to use var due to issue of
 block scope or functional scope..
 */
 
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// ------------------DATATYPES-----------
+// PRIMITIVE -> On the basis of how they are stored and accessed from memory
+
+// Primitive are call by value
+// 7 types :-> String, number, Boolean, BigInt, null,undefined, Symbol
+
+// Non-Primitive OR Reference type
+// Arrays, Objects , Functions
+
+//  Declaring Symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')   // For uniqueness
+
+// console.log(id === anotherId)
+
+// Declaring Reference Types
+
+const heroes = ["IronMan", "Thor", "Hulk"];
+
+let IronDetails = {
+    name : "Tony Stark" ,
+    character: "IRON MAN"  
+}
+
+const func = function(){
+    console.log("K");
+}
+
+// TypeOf null is object
