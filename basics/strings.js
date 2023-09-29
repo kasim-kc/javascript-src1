9b883c73a6e04b72310233ddf0928977e8f2b961
kasim-kc <133113806+kasim-kc@users.noreply.github.com>
2023-09-29 07:19:20 +0000

    Started Strings

---
+++ basics/strings.js
@@ -0,0 +1,30 @@
+// -------------- STACK AND HEAP MEMORY-----
+//  Primitive datatypes get memory in Stack..
+// Non-Primitive datatypes get memory in Heap..
+
+let score = 33;
+let nextScore = score;
+
+//console.log(nextScore)
+nextScore = 10;
+// console.log(nextScore)
+// console.log(score)         // nextScore is given a copy of score variable and  is not actually referencing score
+
+let user = {
+    username : "Kasim-kc",
+    pass : "KASIMFOR10"
+}
+
+let userOne = user;
+// console.log(userOne);
+
+// userOne.username = "kas-c";
+// console.log(user);
+
+
+// ----------------------************ STRINGS *******---------------\
+
+// Concatenation of Strings Using String Interpolation(` `)
+let name = "Kasim";
+let age = 21;
+console.log(`Hello my name is ${name} and my age is ${age}`);    // This method takes help of placeholders and inject values them.
