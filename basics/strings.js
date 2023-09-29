15acf260b7e2b73326bcc0e8441b68ba430f992f
kasim-kc <133113806+kasim-kc@users.noreply.github.com>
2023-09-29 10:29:40 +0000

    strings start

--- basics/strings.js
+++ basics/strings.js
@@ -27,4 +27,14 @@ let userOne = user;
 // Concatenation of Strings Using String Interpolation(` `)
 let name = "Kasim";
 let age = 21;
-console.log(`Hello my name is ${name} and my age is ${age}`);    // This method takes help of placeholders and inject values them.
+// console.log(`Hello my name is ${name} and my age is ${age}`);    // This method takes help of placeholders and inject values them.
+
+
+// Another way to declare a string..
+const gameName = new String('kasimkc');     // String as Object
+// console.table([typeof gameName, typeof name]);
+
+// console.log(gameName[1]);     // Accessing an index of a string
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
